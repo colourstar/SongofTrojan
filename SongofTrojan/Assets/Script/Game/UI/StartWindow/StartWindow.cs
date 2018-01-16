aa9d7719ac02f66d9842721717edba9c3080a6ed
colourstar <colourstar27@qq.com>
2018-01-16 16:41:14 +0800

    支持开场动画 支持切换地图

--- SongofTrojan/Assets/Script/Game/UI/StartWindow/StartWindow.cs
+++ SongofTrojan/Assets/Script/Game/UI/StartWindow/StartWindow.cs
@@ -3,18 +3,32 @@ using System.Collections;
 
 public class StartWindow : UIWindowBase 
 {
-
+    private float m_iCurrentTime = 0.0f;
     //UI的初始化请放在这里
     public override void OnOpen()
     {
-		AddOnClickListener ("btlogin", OnStartClick);
-
+        StartCoroutine(TextAnim(GetGameObject("Text"), 1, 0));
     }
 
     //请在这里写UI的更新逻辑，当该UI监听的事件触发时，该函数会被调用
     public override void OnRefresh()
     {
 
+    }
+
+    public void Update()
+    {
+        m_iCurrentTime += Time.deltaTime;
+        float fAlpha = (m_iCurrentTime % 3.0f) / 3.0f;
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            OnStart();
+        }
     }
 
     //UI的进入动画
@@ -27,23 +41,22 @@ public class StartWindow : UIWindowBase
 
         yield return new WaitForEndOfFrame();
     }
-
-    //UI的退出动画
-//    public override IEnumerator ExitAnim(UIAnimCallBack l_animComplete, UICallBack l_callBack, params object[] objs)
-//    {
-//        AnimSystem.UguiAlpha(gameObject , null, 0, callBack:(object[] obj) =>
-//        {
-//            StartCoroutine(base.ExitAnim(l_animComplete, l_callBack, objs));
-//        });
-//
-//        yield return new WaitForEndOfFrame();
-//    }
-
-	// 按钮消息接受
-	public void OnStartClick(InputUIOnClickEvent e)
+        
+	// 按键消息接受
+	public void OnStart()
 	{
 		ApplicationStatusManager.GetStatus<StartStatus>().CloseUI<StartWindow>();
 		ApplicationStatusManager.EnterStatus<GameStatus>();
-//		ApplicationStatusManager.GetStatus<StartStatus>().CloseUI<StartWindow>();
 	}
+
+    // 动态效果
+    public IEnumerator TextAnim(GameObject gameobject,float fromvalue,float tovalue)
+    {
+        AnimSystem.UguiAlpha(gameobject, fromvalue, tovalue, callBack:(object[] obj)=>
+            {
+                StartCoroutine(TextAnim(gameobject, tovalue, fromvalue));
+            });
+
+        yield return new WaitForEndOfFrame();
+    }
 }
